7b9485ec9cf5ccffb38dad3053a16df1f40ae598
Mikhail Cheshkov <mcheshkov@gmail.com>
2021-02-14 18:19:01 +0300

    Add score printing rate divider

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -5,13 +5,18 @@ import org.optaplanner.core.api.solver.SolverFactory
 fun main(args: Array<String>) {
     // Path relative to src/main/kotlin/resources
     val solver = SolverFactory.createFromXmlResource<Album>("BaseSolverConfig.xml").buildSolver()
+
+    var iter = 0
+    val mult = 1
     solver.addEventListener {
         val solution = it.newBestSolution
         val score = solution.score
         if (score !== null && score.isFeasible) {
-            println("Score: $score")
-            println(solution.toString())
+            if (iter % mult == 0) println("Score: $score")
+//            println(solution.toString())
         }
+
+        iter += 1
     }
 
     // Path relative to cwd
